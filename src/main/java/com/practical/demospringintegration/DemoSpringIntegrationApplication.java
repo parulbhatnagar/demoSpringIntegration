3eb620e4c922a44699a17b676807fddb8b2d0aef
ParulBhatnagar <parulbhatnagar@gmail.com>
2024-05-26 08:22:24 +0530

    initial checkin

---
+++ src/main/java/com/practical/demospringintegration/DemoSpringIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.practical.demospringintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/practical/demospringintegration/DemoSpringIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practical.demospringintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
